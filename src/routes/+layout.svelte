506f8a541d978981f59674669cf025aaefc161c6
Anthony <anthony.matignon@gmail.com>
2025-09-12 14:36:30 +0200

    Ajout de la gestion conditionnelle des scripts d'analyse pour le suivi des visiteurs en production.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,19 +3,32 @@
 	import favicon from '$lib/assets/favicon.jpg';
 	import Header from '$lib/components/layout/Header.svelte';
 	import Footer from '$lib/components/layout/Footer.svelte';
+	import { browser, dev } from '$app/environment';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	{#if browser && !dev}
+		<script
+			defer
+			src="https://umami.amatignon.fr/script.js"
+			data-website-id="57bc1b7b-6e09-43f9-8fad-e8509503f25e"
+		></script>
+		<script
+			defer
+			data-domain="amatignon.fr"
+			src="https://plausible.amatignon.fr/js/script.outbound-links.js"
+		></script>
+	{/if}
 </svelte:head>
 
-<div class="bg-slate-900 text-white min-h-dvh">
+<div class="min-h-dvh bg-slate-900 text-white">
 	<div class="container mx-auto">
 		<Header />
 		{@render children?.()}
-
 	</div>
 	<Footer />
 </div>
