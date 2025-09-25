18f51e4aa0bddb869ddcbfbf5bb3f58c13401781
Anthony <anthony.matignon@gmail.com>
2025-09-25 15:49:16 +0200

    Suppression de la journalisation de la locale dans le chargement des articles

--- src/routes/articles/+page.server.ts
+++ src/routes/articles/+page.server.ts
@@ -4,7 +4,6 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
 	const locale = getLocale();
-	console.log(locale);
 	const articles = await ListArticlesByLang(locale);
 	return { articles };
 };
